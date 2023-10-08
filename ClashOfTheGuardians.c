e0db8e3d195c035014e0370e52f6d67d7a805ac7
ByShisho <shishoisworking@gmail.com>
2023-10-08 17:45:22 -0300

    Clash of the guardians 2

--- ClashOfTheGuardians.c
+++ ClashOfTheGuardians.c
@@ -7,6 +7,7 @@
 //Iniciar estructura de las cartas
 typedef struct cartas{
 	
+	int CardCode;
 	char *name;
 	char *type;
 	int HP;
@@ -25,7 +26,7 @@ typedef struct jugadores{
 }Player;
 
 	//Menú de inicio
-int GameMenu(){
+int GameMenu(int cartas){
 	
 	int opcion;
 	
@@ -33,7 +34,8 @@ int GameMenu(){
 		printf("|CLASH OF THE GUARDIANS|\n");
 		printf("-----------------------\n");
 		printf("\n");
-		
+				
+		printf("Cartas activas: %d", cartas);
 
 		printf("	>1< Agregar una carta a la lista\n");
 		printf("	>2< Iniciar partida\n");
@@ -45,7 +47,7 @@ int GameMenu(){
 	return opcion;
 }
 
-int NewCard(){
+int NewCard(int code){
 	
 	FILE *file;
 	file = fopen("CARDS.txt", "ab");
@@ -59,6 +61,7 @@ int NewCard(){
 	Mazo.name = malloc(60);
 	Mazo.type = malloc(40);
 	
+	Mazo.CardCode = code+1;
 	printf("Ingrese el nombre de la carta: ");	fgets(Mazo.name, 60, stdin);	fflush(stdin);	printf("\n");
 	printf("Ingrese el tipo de guardian: ");	fgets(Mazo.type, 40, stdin); 	fflush(stdin); 	printf("\n");
 	printf("Ingrese el HP: "); 					scanf("%d", &Mazo.HP);			fflush(stdin);	printf("\n");
@@ -73,12 +76,11 @@ int NewCard(){
 	fclose(file);
 }
 
-int main(){
+//Regarcar el mazo para cada vez que se añadan cartas
+int DeckBuild(){
 	
-	int read = 0, cardload = 0;
-	int winner = 0;
-	int opcion;
-	
+	int cardload = 0;
+	int read = 0;
 	//Lectura del archivo con las cartas
 	FILE *file;	
 	file = fopen("CARDS.txt", "r");
@@ -90,49 +92,70 @@ int main(){
 	//Iniciar arreglo con el mazo
 	Guardians Mazo[100];
 	//Carga correcta de las cartas	
+	
 	do{
-		read = fscanf(file,
-						"%59[^,], %39[^,], %d, %d, %d\n",
+		
+ 	read =	fscanf(file,"%d ,%99[^,] ,%99[^,] ,%d ,%d ,%d\n",
+						&Mazo[cardload].CardCode,
 						Mazo[cardload].name,
 						Mazo[cardload].type,
 						&Mazo[cardload].HP,
 						&Mazo[cardload].DMG,
 						&Mazo[cardload].DEF);
-		
-		if(read == 5) cardload++;
+						
+	if(read == 6) cardload++;
+							
 		
 	}while(!feof(file));
 	
+	fclose(file);
+	return cardload;
+}
+
+int main(){
 	
-	while(winner != 1 || winner != 2){ //Hasta ver que jugador gana
+	
+	DeckBuild();
+	printf("peo");
+	
+	int read = 0, Cartasactivas;
+	int winner = 0;
+	int opcion;
+	
+	
+	while(winner != 1 && winner != 2){ //Hasta ver que jugador gana
 	
 	//Presentación del juego
-		GameMenu();
+		DeckBuild();
+		GameMenu(Cartasactivas);
 		
-			opcion = GameMenu();
+			opcion = GameMenu(Cartasactivas);
 		
 		switch(opcion){
 			
 			case 1:	//Añadir una carta
-				system("cls");
-				NewCard();
+				NewCard(Cartasactivas);
+				//Carga del mazo actualizado y la cantidad de cartas activas
+				Cartasactivas = DeckBuild();
+				
 				break;
 			
-			case 2:	// Iniciar el juego
+			//case 2:	// Iniciar el juego
 				
-				break;
+			//	break;
 				
-			case 3:	//Ver el historialde ganadores
+			//case 3:	//Ver el historialde ganadores
 				
-				break;
+			//	break;
 				
-			case 4:	//Salir
+			case 2:	//Salir
 				printf("Vuelve pronto :D");
 				return 0;
 				break;
+			}
 		}
 		 
-	}	
+		
 	
 	return 0;
 }
